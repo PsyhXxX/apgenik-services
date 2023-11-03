84e6d64c8af29c46ac3d07cd93654c42e6a41890
A. G. Nikitin <ars_nikitin@hotmail.com>
2023-11-03 23:30:17 +0300

    First commit

---
+++ src/main/java/ru/apgenik/apgenikservices/ApgenikServicesApplication.java
@@ -0,0 +1,13 @@
+package ru.apgenik.apgenikservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApgenikServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApgenikServicesApplication.class, args);
+	}
+
+}
